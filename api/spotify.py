cdf3485febd74c49b381620a1577aa2d8a7a04a8
9ir3s <60795933+9ir3s@users.noreply.github.com>
2025-09-12 01:30:02 -0300

    Update logic for handling song playback status
    Refactor condition to check if a song is playing and update recent plays retrieval.

--- api/spotify.py
+++ api/spotify.py
@@ -120,14 +120,16 @@ def makeSVG(data, background_color, border_color):
     contentBar = "".join(["<div class='bar'></div>" for _ in range(barCount)])
     barCSS = barGen(barCount)
 
-    if data == {}:
-        #contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
-        recentPlays = recentlyPlayed()
+    if not "is_playing" in data:
+        contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
+        currentStatus = "Recently played:"
+        recentPlays = get(RECENTLY_PLAYING_URL)
         recentPlaysLength = len(recentPlays["items"])
         itemIndex = random.randint(0, recentPlaysLength - 1)
         item = recentPlays["items"][itemIndex]["track"]
     else:
         item = data["item"]
+        currentStatus = "Last seen playing:"
 
     if item["album"]["images"] == []:
         image = PLACEHOLDER_IMAGE
